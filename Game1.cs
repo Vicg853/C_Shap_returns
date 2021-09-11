19577979deb817291b978eb85f4d68c1adeedd68
Vicg853 <vicg853@gmail.com>
2021-09-11 20:38:35 -0300

    First commit: started project, added MonoGame packages, started 3D engine based on MonoGame's tools

---
+++ Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace IReallyDN
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+
+        BasicEffect BasicEffect;
+
+        //Geometric info
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
+        //Orbit 
+        bool orbit;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {   
+            base.Initialize();
+
+            //Starting camera setup
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+
+            projectionMatrix = Matrix.CreatePerspective
+        }
+
+        protected override void LoadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
